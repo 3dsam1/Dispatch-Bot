d7fe751a449b221a35c25e2c048b30a5e93f4f64
3dsam1 <56355588+3dsam1@users.noreply.github.com>
2021-03-03 19:13:47 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const fs = require('fs');
 
 
-let token = '' // Your token here
+let token = 'YOUR TOKEN HERE'
 
 const client = new Discord.Client();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -54,21 +54,115 @@ const args = message.content.slice(prefix.length).split(/ +/);
 const command = args.shift(). toLowerCase(); 
 
 
-//Test
+
 
 
 
 
 
 if(command === 'ping'){
-   
-    client.commands.get('ping').execute(message, args);
+    message.channel.send(new Discord.MessageEmbed().setTitle("Pinging...")
+    .setDescription("Please wait.")).then(pingMessage => {
+        var ping = pingMessage.createdTimestamp - message.createdTimestamp;
+
+        pingMessage.edit(new Discord.MessageEmbed().setTitle("Latency :ping_pong:")
+            .setDescription(`Bot: ${ping} ms\nAPI: ${client.ws.ping} ms`))
+        .then
+    });
+    
 
 }
 
 
 else if(command === 'addpd'){
-client.commands.get('addpd').execute(message, args);
+    const userName = args.join(" ").split(22)
+
+
+if(onlinePD == undefined){
+    onlinePD = userName;
+}
+else {onlinePD = onlinePD + `\n ${userName}`
+}
+channel = message.channel;
+
+
+
+channel.messages.fetch('816533944329175040')
+
+.then(message => message.edit(
+    new Discord.MessageEmbed()
+.setTitle('CURRENT ACTIVE USERS')
+.setDescription(`**ONLINE POLICE** \n${onlinePD}\n\n **ONLINE EMS**\n${onlineEMS}`)
+.setColor('RED'))
+)
+
+
+message.reply(`${userName} is now On duty.`).then(message => message.delete({ timeout: 10000}))
+message.delete({ timeout: 1000})
+
+console.log(onlinePD)
+}
+
+
+else if(command === 'addems'){
+    const userName = args.join(" ").split(22)
+
+
+if(onlineEMS == undefined){
+    onlineEMS = userName;
+}
+
+else {onlineEMS = onlineEMS + `\n ${userName}`
+}
+channel = message.channel;
+
+
+
+channel.messages.fetch('816533944329175040')
+.then(message => message.edit(
+    new Discord.MessageEmbed()
+.setTitle('CURRENT ACTIVE USERS')
+.setDescription(`**ONLINE POLICE** \n${onlinePD}\n\n **ONLINE EMS**\n${onlineEMS}`)
+.setColor('RED'))
+)
+
+console.log(onlineEMS)
+
+message.reply(`${userName} is now On Duty.`).then(message => message.delete({ timeout: 10000}))
+message.delete({ timeout: 1000})
+
+}
+
+else if(command === 'removepd'){
+
+    const userName = args.join(" ").split(22)
+
+    onlinePD = onlinePD - userName;
+  
+        
+   
+
+
+
+
+    channel = message.channel;
+
+
+
+channel.messages.fetch('816533944329175040')
+.then(message => message.edit(
+    new Discord.MessageEmbed()
+.setTitle('CURRENT ACTIVE USERS')
+.setDescription(`**ONLINE POLICE** \n${onlinePD}\n\n **ONLINE EMS**\n${onlineEMS}`)
+.setColor('RED'))
+)
+
+console.log(onlinePD)
+
+message.reply(`${userName} is now Off Duty.`).then(message => message.delete({ timeout: 10000}))
+message.delete({ timeout: 1000})
+
+
 
 }
 })
